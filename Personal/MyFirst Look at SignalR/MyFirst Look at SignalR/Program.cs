7dd9bb9e0c07c9f4f15ddfebb9e9be7327b7ad78
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-16 11:46:06 +0700

    Added a new Soultion.

---
+++ Personal/MyFirst Look at SignalR/MyFirst Look at SignalR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirst_Look_at_SignalR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
