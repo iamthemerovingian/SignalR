19b944bd9210a4e1ade31735f35dbd515ce19e8d
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-16 14:48:20 +0700

    Created first hub with Send Message methods that accept stings and complex types.
    Added a OWIN startup class for the routes.

--- Personal/SimpleChat/SimpleChat/ChatHub.cs
+++ Personal/SimpleChat/SimpleChat/ChatHub.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace SimpleChat
 {
+    [HubName("chat")]
     public class ChatHub : Hub
     {
-        public void Hello()
+        public void SendMessage(string message)
         {
-            Clients.All.hello();
+            Clients.All.newMessage(message);
         }
+
+        public void SendMessageData(SendData data)
+        {
+            // process incoming data..
+            // transform data...
+            // craft new data..
+
+            Clients.All.newData(data);
+        }
+
+        //public Task<int> SendDataAsync()
+        //{
+        //    //async work...
+        //}
+    }
+
+    public class SendData
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
     }
 }
---
+++ Personal/SimpleChat/SimpleChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SimpleChat.Startup))]
+namespace SimpleChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
