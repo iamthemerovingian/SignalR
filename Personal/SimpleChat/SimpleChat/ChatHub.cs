2efcd83087677ee2ca0e93c66eb95705a5dc6d3e
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-16 16:39:23 +0700

    Added Capability to support groups of clients. Added capability to support tracking clients connecting, disconnecting or reconnecting. Added a monitor hub to show how we can call client size methods through the hub class.
    Completed the Server side implementation of the SignalR Hub.

--- Personal/SimpleChat/SimpleChat/ChatHub.cs
+++ Personal/SimpleChat/SimpleChat/ChatHub.cs
@@ -13,14 +13,28 @@ namespace SimpleChat
     {
         public void SendMessage(string message)
         {
-            Clients.Caller.newMessage(message);
-            Clients.Client(Context.ConnectionId).newMessage(message); // Same as above. 
+            //Clients.Caller.newMessage(message);
+            //Clients.Client(Context.ConnectionId).newMessage(message); // Same as above. 
 
-            Clients.All.newMessage(message); // Everyone
-            Clients.Others.newMessages(message); // Everyone other than me.
-            Clients.AllExcept(Context.ConnectionId).newMessage(message); // Everyone other than specific clients.
+            //Clients.All.newMessage(message); // Everyone
+            //Clients.Others.newMessages(message); // Everyone other than me.
+            //Clients.AllExcept(Context.ConnectionId).newMessage(message); // Everyone other than specific clients.
+
+            var msg = String.Format("{0}: {1}", Context.ConnectionId, message);
 
             Clients.All.newMessage(message);
+        }
+
+        public void JoinRoom(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+        }
+
+        public void SendMessageToRoom(string room, string message)
+        {
+            var msg = String.Format("{0}: {1}", Context.ConnectionId, message);
+
+            Clients.Group(room).newMessage(msg);
         }
 
         public void SendMessageData(SendData data)
@@ -36,6 +50,31 @@ namespace SimpleChat
         //{
         //    //async work...
         //}
+
+        public override Task OnConnected()
+        {
+            SendMonitoringData("Connected", Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            SendMonitoringData("Disconnected", Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            SendMonitoringData("Reconnected", Context.ConnectionId);
+            return base.OnReconnected();
+        }
+
+
+
+        private void SendMonitoringData(string eventType, string connectionId)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<MonitorHub>();
+            context.Clients.All.newEvent(eventType, connectionId);
+        }
     }
 
     public class SendData
