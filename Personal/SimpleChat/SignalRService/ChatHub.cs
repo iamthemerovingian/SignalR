d9172f8ebe5a63020c0f2bc61c452e0ec98aa3d3
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-17 16:19:55 +0700

    Moved the Hubs from the WebApp to a sperate Assembly. Added nuget package for Siglar to the new project.

---
+++ Personal/SimpleChat/SignalRService/ChatHub.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+
+namespace SimpleChat
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            //Clients.Caller.newMessage(message);
+            //Clients.Client(Context.ConnectionId).newMessage(message); // Same as above. 
+
+            //Clients.All.newMessage(message); // Everyone
+            //Clients.Others.newMessages(message); // Everyone other than me.
+            //Clients.AllExcept(Context.ConnectionId).newMessage(message); // Everyone other than specific clients.
+
+            var msg = String.Format($"{Context.ConnectionId.ToString()} : {message}");
+
+            Clients.All.newMessage(msg);
+        }
+
+        public void JoinRoom(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+        }
+
+        public void SendMessageToRoom(string room, string message)
+        {
+            var msg = String.Format($"{Context.ConnectionId} : {message}");
+
+            Clients.Group(room).newMessage(msg);
+        }
+
+        public void SendMessageData(SendData data)
+        {
+            // process incoming data..
+            // transform data...
+            // craft new data..
+
+            Clients.All.newData(data);
+        }
+
+        //public Task<int> SendDataAsync()
+        //{
+        //    //async work...
+        //}
+
+        public override Task OnConnected()
+        {
+            SendMonitoringData("Connected", Context.ConnectionId);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            SendMonitoringData("Disconnected", Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            SendMonitoringData("Reconnected", Context.ConnectionId);
+            return base.OnReconnected();
+        }
+
+
+
+        private void SendMonitoringData(string eventType, string connectionId)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<MonitorHub>();
+            context.Clients.All.newEvent(eventType, connectionId);
+        }
+    }
+
+    public class SendData
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+    }
+}
